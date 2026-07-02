75295dd54c476298324e173d8cd93a8d8774cc42
Frozen Forest Reality Technologies <frozenforeststudio@gmail.com>
2026-07-02 22:25:45 +0300

    Amalgamation to StaticLib Change
    We did this change to use pugixml as dependency for other plugins.

--- Source/FF_PugiXml/FF_PugiXml.Build.cs
+++ Source/FF_PugiXml/FF_PugiXml.Build.cs
@@ -1,5 +1,3 @@
-// Some copyright should be here...
-
 using System;
 using System.IO;
 using UnrealBuildTool;
@@ -13,12 +11,28 @@ public class FF_PugiXml : ModuleRules
         CppCompileWarningSettings.UndefinedIdentifierWarningLevel = WarningLevel.Off;
         bEnableExceptions = true;
 
+        string Path_Source = Path.Combine(ModuleDirectory, "ThirdParty", "pugixml");
+
         if (UnrealTargetPlatform.Win64 == Target.Platform)
 		{
             bUseRTTI = true;
-        }
+           
+            string Windows_Source = Path.Combine(Path_Source, "Win64");
 
-        PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "ThirdParty", "pugixml"));
+            string Windows_Include = Path.Combine(Windows_Source, "include");
+            PublicIncludePaths.Add(Windows_Include);
+
+            string Windows_Libs = Path.Combine(Windows_Source, "lib");
+            string[] List_Libs = Directory.GetFiles(Windows_Libs, "*.lib", SearchOption.TopDirectoryOnly);
+
+            foreach (string File in List_Libs)
+            {
+                if (File.EndsWith(".lib"))
+                {
+                    PublicAdditionalLibraries.Add(File);
+                }
+            }
+        }
 
         PublicDependencyModuleNames.AddRange(
 			new string[]
